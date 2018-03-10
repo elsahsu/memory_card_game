bebe42a809215866668f6b9ff6203460fb7075f4
Elsa Hsu <elsanfhsu@gmail.com>
2018-03-10 21:29:02 +0200

    popup message

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-//lists of cards
+//variables
 var open_cards = [];
 var matched_cards = [];
 var movecount = 0;
 var seconds = 0;
 var minutes = 0;
+var stars = 0;
 
-// Shuffle
+//Shuffle
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -20,6 +21,7 @@ function shuffle(array) {
     return array;
 }
 
+//show & pair cards, count moves, remove stars
 function show_card_symbol(card) {
   card.className += " open";
   card.className += " show";
@@ -31,10 +33,13 @@ function show_card_symbol(card) {
     console.log(movecount);
     $(".moves").html(movecount);
       if (movecount === 10) {
+        stars = 2;
         $('#star-2').remove();
       } else if (movecount === 15) {
+        stars = 1;
         $('#star-1').remove();
       } else if (movecount === 20) {
+        stars = 0;
         $('#star-0').remove();
       }
     for (var i = 0; i < open_cards.length; i++) {
@@ -46,7 +51,13 @@ function show_card_symbol(card) {
         matched_cards.push(card);
         matched_cards.push(open_card);
         if (matched_cards.length === 16) {
+            let modal = document.getElementById('win-dialog');
             clearInterval(myTimer);
+            modal.style.display = "inline-block";
+            for (let i = 0; i < stars; i++) {
+                $("#popup-stars").append('<li><i class="fa fa-star"></i></li>');
+            };
+            $("#popup-time").html(minutes + ':' + seconds);
         }
         open_cards.pop();
         return;
@@ -69,6 +80,7 @@ function show_card_symbol(card) {
   console.log(open_cards);
 };
 
+//timer
 function add_second() {
   seconds = seconds + 1;
   if (seconds === 60) {
@@ -78,13 +90,19 @@ function add_second() {
   $(".seconds").html(minutes + ':' + seconds);
   console.log('adding seconds succeed!');
 }
-var myTimer = setInterval(add_second, 1000);
+var myTimer;
 
+//start a game
 function start_new_game() {
+  stars = 3;
+  movecount = 0;
+  seconds = 0;
+  minutes = 0;
+  $(".seconds").html(minutes + ':' + seconds);
   $(".deck").empty();
   $(".stars").empty();
-  movecount = 0;
   $(".moves").html(movecount);
+  myTimer = setInterval(add_second, 1000);
   var all_cards = ["anchor", "anchor", "bicycle", "bicycle", "bolt", "bolt", "bomb", "bomb", "cube", "cube", "diamond", "diamond", "leaf", "leaf", "paper-plane", "paper-plane"]
   var shuffled_cards = shuffle(all_cards);
   console.log(shuffled_cards);
@@ -92,39 +110,39 @@ function start_new_game() {
     var imagename = shuffled_cards[i];
     $(".deck").append('<li class="card"><i class="fa fa-' + imagename + '"></i></li>');
   }
+
+  //addEventListener to cards
+  let cardlist = document.querySelectorAll('.card');
+  for (var i = 0; i < cardlist.length; i++) {
+    var card = cardlist[i];
+    card.addEventListener('click', function(event) {
+      console.log('The card was clicked!');
+  // console.log(event.target);
+      show_card_symbol(event.target);
+  //    event.target.className += " open";
+  //    event.target.className += " show";
+  })};
+
   for (var i = 0; i < 3; i++) {
     $(".stars").append('<li id="star-'+ i +'"><i class="fa fa-star"></i></li>');
   }
 }
-
 start_new_game();
 
-//var.addEventListener('click', function(event){})-->JS
-//$(".selector").click(function(){})-->jQuery
-//restart
+//restart button
 $(".fa-repeat").click(function() {
   console.log('restart button works asap!')
   start_new_game();
 });
 
-const cardlist = document.querySelectorAll('.card');
-for (var i = 0; i < cardlist.length; i++) {
-  var card = cardlist[i];
-  card.addEventListener('click', function(event) {
-    console.log('The card was clicked!');
-// console.log(event.target);
-    show_card_symbol(event.target);
-//    event.target.className += " open";
-//    event.target.className += " show";
-})};
+//var.addEventListener('click', function(event){})-->JS
+//$(".selector").click(function(){})-->jQuery
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+//popup modal
+let span = document.getElementById("popup-close");
+span.onclick = function() {
+  let windialog = document.getElementById('win-dialog');
+  windialog.style.display = "none";
+  console.log("close the popup!");
+}
